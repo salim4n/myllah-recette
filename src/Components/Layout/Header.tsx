f86933ec2d186de61e6dd40cd9b895ca2b2e9f7e
Salim Laimeche <salim4n@live.fr>
2023-04-22 18:42:23 +0200

    layout button

--- src/Components/Layout/Header.tsx
+++ src/Components/Layout/Header.tsx
@@ -5,7 +5,19 @@ function Header() {
 	return (
 		<nav className="navbar sticky-top navbar-light  back-taupe rounded-pill pt-4">
 			<NavLink className="nav-link  p-2 " aria-current="page" to="/">
-				<h3 className="blanc hoverable">Myllah-Recettes</h3>
+				<button className="btn btn-lg hoverable back-blanc rounded-pill">
+					{" "}
+					<h3 className="taupe ">Myllah-Recettes</h3>
+				</button>
+			</NavLink>
+			<NavLink
+				className="nav-link  p-2 "
+				aria-current="page"
+				to="/createRecipe">
+				<button className="btn btn-lg hoverable back-blanc rounded-pill">
+					{" "}
+					<h3 className="taupe ">Créer une Recette</h3>
+				</button>
 			</NavLink>
 		</nav>
 	);
